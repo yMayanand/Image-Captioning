8eed4b25ca8f383541440347112ba42d1084a2f5
thinkin-machine <you@example.com>
2022-11-01 19:59:22 +0530

    fix bug

--- dataset.py
+++ dataset.py
@@ -5,20 +5,21 @@ import pandas as pd
 from preprocess import read_file
 
 class CaptionDataset(torch.utils.data.Dataset):
-    def __init__(self, fname, transform=None):
+    def __init__(self, fname, tokenizer, transform=None):
         self.root = fname.split('/')[0]
         self.tfms = transform
         super().__init__()
         temp = read_file(fname)
         self.df = pd.DataFrame(temp, columns=['id'])
+        self.tokenizer = tokenizer
 
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):
         image_path = self.df['id'][idx]
-        caption = tokenizer.caption_df[tokenizer.caption_df['id']==image_path].reset_index(drop=True)['caption'][0].lower().strip().split()
-        caption = [tokenizer.val2idx[i] for i in caption]
+        caption = self.tokenizer.caption_df[self.tokenizer.caption_df['id']==image_path].reset_index(drop=True)['caption'][0].lower().strip().split()
+        caption = [self.tokenizer.val2idx[i] for i in caption]
         image = Image.open(os.path.join(self.root, 'Flicker8k_Dataset', image_path))
         if self.tfms:
             image = self.tfms(image)
--- train.py
+++ train.py
@@ -40,8 +40,8 @@ def train_model():
     encoder = Encoder().to(device)
     decoder = Decoder(tokenizer).to(device)
 
-    train_ds = CaptionDataset(os.path.join(args.root_dir, 'Flicker8k_text/Flickr_8k.trainImages.txt'), transform=tfms)
-    val_ds = CaptionDataset(os.path.join(args.root_dir, 'Flicker8k_text/Flickr_8k.devImages.txt'), transform=tfms)
+    train_ds = CaptionDataset(os.path.join(args.root_dir, 'Flicker8k_text/Flickr_8k.trainImages.txt'), tokenizer, transform=tfms)
+    val_ds = CaptionDataset(os.path.join(args.root_dir, 'Flicker8k_text/Flickr_8k.devImages.txt'), tokenizer, transform=tfms)
 
     train_dl = torch.utils.data.DataLoader(train_ds, batch_size=32, shuffle=True, pin_memory=True, num_workers=2, collate_fn=cust_collate)
     val_dl = torch.utils.data.DataLoader(val_ds, batch_size=32, pin_memory=True, num_workers=2, collate_fn=cust_collate)
