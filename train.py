441c7405d9149ee8e977f76804bf92c54aaf5bb5
thinkin-machine <you@example.com>
2022-11-03 17:19:53 +0530

    add ddp training

--- train.py
+++ train.py
@@ -13,6 +13,11 @@ from utils import cust_collate
 from tqdm.notebook import tqdm
 from argparse import ArgumentParser
 
+import torch.multiprocessing as mp
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def load_from_ckpt(encoder, decoder, path):
@@ -51,7 +56,7 @@ def train_model():
     #param_groups = [{'params': encoder.parameters(), 'lr': 1e-5},
                     #{'params': decoder.parameters(), 'lr': 1e-2}]
 
-    optimizer = torch.optim.Adam(params, lr=args.lr)
+    optimizer = optim.Adam(params, lr=args.lr)
     #sched = optim.lr_scheduler.OneCycleLR(optimizer, max_lr=0.001, steps_per_epoch=len(train_dl), epochs=10)
     epochs = args.epochs
     loss_store = []
@@ -77,8 +82,114 @@ def train_model():
     plt.plot(loss_store)
     plt.savefig(os.path.join(args.root_dir, 'loss.png'))
 
+def ddp_setup():
+    init_process_group(backend="nccl")
+
+class Trainer:
+    def __init__(
+        self,
+        root_dir,
+        lr,
+        save_every,
+        snapshot_path,
+    ) -> None:
+        self.local_rank = int(os.environ["LOCAL_RANK"])
+        self.global_rank = int(os.environ["RANK"])
+        self.save_every = save_every
+        self.epochs_run = 0
+        self.snapshot_path = snapshot_path
+        if os.path.exists(snapshot_path):
+            print("Loading snapshot")
+            self._load_snapshot(snapshot_path)
+
+        tfms = transforms.Compose([
+            transforms.Resize((400, 400)),
+            transforms.ToTensor()
+        ])
+
+        tokenizer = Tokenizer(root_dir)
+        tokenizer.tokenize(os.path.join(root_dir, 'Flicker8k_text/Flickr_8k.trainImages.txt'))
+
+        self.encoder = Encoder().to(device)
+        self.decoder = Decoder(tokenizer).to(device)
+
+        self.decoder = DDP(self.decoder, device_ids=[self.local_rank])
+
+        params = list(self.decoder.parameters())
+        self.optimizer = optim.Adam(params, lr=lr)
+
+        self.criterion = CaptionLoss(self.decoder, 0.5, tokenizer)
+
+        train_ds = CaptionDataset(root_dir, 'Flicker8k_text/Flickr_8k.trainImages.txt', tokenizer, transform=tfms)
+        val_ds = CaptionDataset(root_dir, 'Flicker8k_text/Flickr_8k.devImages.txt', tokenizer, transform=tfms)
+
+        self.train_dl = torch.utils.data.DataLoader(train_ds, batch_size=32, pin_memory=True, shuffle=False, sampler=DistributedSampler(dataset), num_workers=2, collate_fn=cust_collate)
+        val_dl = torch.utils.data.DataLoader(val_ds, batch_size=32, pin_memory=True, num_workers=2, collate_fn=cust_collate)
+
+    def _load_snapshot(self, snapshot_path):
+        loc = f"cuda:{self.gpu_id}"
+        snapshot = torch.load(snapshot_path, map_location=loc)
+        self.model.load_state_dict(snapshot["MODEL_STATE"])
+        self.epochs_run = snapshot["EPOCHS_RUN"]
+        print(f"Resuming training from snapshot at Epoch {self.epochs_run}")
+
+    def _run_batch(self, source, targets):
+        self.optimizer.zero_grad()
+        output = self.encoder(source)
+        loss = self.criterion(output, targets)
+        loss.backward()
+        self.optimizer.step()
+        return loss.item()
+
+    def _run_epoch(self, epoch):
+        b_sz = len(next(iter(self.train_dl))[0])
+        print(f"[GPU{self.global_rank}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}")
+        self.train_dl.sampler.set_epoch(epoch)
+        loss_store = []
+        for source, targets in self.train_dl:
+            source = source.to(self.local_rank)
+            targets = targets.to(self.local_rank)
+            loss = self._run_batch(source, targets)
+            loss_store.append(loss)
+        avg_loss = np.mean(loss_store)
+        print(f"Epoch {epoch} Loss: {avg_loss}")
+
+    def _save_snapshot(self, epoch):
+        snapshot = {
+            "MODEL_STATE": self.model.module.state_dict(),
+            "EPOCHS_RUN": epoch,
+        }
+        torch.save(snapshot, self.snapshot_path)
+        print(f"Epoch {epoch} | Training snapshot saved at {self.snapshot_path}")
+
+    def train(self, max_epochs: int):
+        for epoch in range(self.epochs_run, max_epochs):
+            self._run_epoch(epoch)
+            if self.local_rank == 0 and epoch % self.save_every == 0:
+                self._save_snapshot(epoch)
+
+
+
+def main(root_dir, lr, save_every, total_epochs, snapshot_path):
+    ddp_setup()
+    trainer = Trainer(root_dir, lr, save_every, snapshot_path)
+    trainer.train(total_epochs)
+    destroy_process_group()
+
+
 if __name__ == '__main__':
-    train_model()
+    parser = ArgumentParser()
+    parser.add_argument("--root_dir", default="./", type=str, help="root directory path")
+    parser.add_argument("--epochs", default=10, type=int, help="num of epochs to train")
+    parser.add_argument("--lr", default=1e-2, type=float, help="learing rate")
+    parser.add_argument("--save_every", default=2, type=int, help="saving snapshot of model every n epochs")
+    parser.add_argument("--snapshot_path", default='snapshot.pt', type=str, help="path to save snapshot")
+
+
+
+    args = parser.parse_args()
+
+    main(args.root_dir, args.lr, args.save_every, args.epochs, args.snapshot_path)
 
 #load_from_ckpt(encoder, decoder, './checkpoint/caption.pt')
 
