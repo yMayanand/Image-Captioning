7fcb71714d791810ad50e0799392595538abdef1
thinkin-machine <you@example.com>
2022-11-08 12:01:29 +0530

    add fixes

--- loss.py
+++ loss.py
@@ -3,31 +3,30 @@ import torch.nn as nn
 import numpy as np
 
 class CaptionLoss(nn.Module):
-    def __init__(self, teacher_forcing, tokenizer, device):
+    def __init__(self, teacher_forcing, tokenizer):
         super().__init__()
-        self.device = device
         self.teacher_forcing = teacher_forcing
         self.tokenizer = tokenizer
 
-    def forward(self, x, labels, decoder):
+    def forward(self, x, labels, decoder, device):
         loss = 0.
         criterion = nn.CrossEntropyLoss()
         batch_size = x.shape[0]
         for i in range(batch_size):
-            inp = decoder.emb(torch.LongTensor([self.tokenizer.val2idx['START']]).to(self.device)) # start token
+            inp = decoder.emb(torch.LongTensor([self.tokenizer.val2idx['START']]).to(device)) # start token
             
             decoder.init_states(x[i])
             for label in labels[i]:
-                label = label.unsqueeze(0).to(self.device)
+                label = label.unsqueeze(0).to(device)
                 out, *(decoder.hn, decoder.cn) = decoder(inp, decoder.hn, decoder.cn)
                 _, pred = torch.max(out, dim=1)
                 if np.random.random() > self.teacher_forcing:
-                    token = torch.LongTensor([pred.item()]).to(self.device)
+                    token = torch.LongTensor([pred.item()]).to(device)
                     inp = decoder.emb(token)
                 else:
                     inp = decoder.emb(label)
                 loss += criterion(out, label)
-            label = torch.LongTensor([self.tokenizer.val2idx['STOP']]).to(self.device) # stop token
+            label = torch.LongTensor([self.tokenizer.val2idx['STOP']]).to(device) # stop token
             out, *(decoder.hn, decoder.cn) = decoder(inp, decoder.hn, decoder.cn)
             loss += criterion(out, label)
         return loss / batch_size
--- train.py
+++ train.py
@@ -96,7 +96,7 @@ class Model(pl.LightningModule):
         self.train_ds = CaptionDataset(root_dir, 'Flicker8k_text/Flickr_8k.trainImages.txt', self.tokenizer, transform=tfms)
         self.val_ds = CaptionDataset(root_dir, 'Flicker8k_text/Flickr_8k.devImages.txt', self.tokenizer, transform=tfms)
 
-        self.loss_func = CaptionLoss(0.5, self.tokenizer, self.device)
+        self.loss_func = CaptionLoss(0.5, self.tokenizer)
 
     def forward(self, x):
         pass
@@ -109,7 +109,7 @@ class Model(pl.LightningModule):
         # training_step defines the train loop.
         x, y = batch
         out = self.model.encoder(x)
-        loss = self.loss_func(out, y, self.model.decoder)
+        loss = self.loss_func(out, y, self.model.decoder, self.device)
         self.log('train_loss', loss.item())
         return loss
 
@@ -117,7 +117,7 @@ class Model(pl.LightningModule):
         # this is the validation loop
         x, y = batch
         out = self.model.encoder(x)
-        loss = self.loss_func(out, y, self.model.decoder)
+        loss = self.loss_func(out, y, self.model.decoder, self.device)
 
         score_list = []
         bs, dim = out.shape
