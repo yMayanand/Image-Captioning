15e7d169b9e3aff251671943f56f835a6d8c86e8
thinkin-machine <you@example.com>
2022-11-20 18:50:27 +0530

    update dataset

--- dataset.py
+++ dataset.py
@@ -19,18 +19,26 @@ class CaptionDataset(torch.utils.data.Dataset):
         self.df = pd.DataFrame(temp, columns=['id'])
         self.tokenizer = tokenizer
 
+        if mode == 'train':
+            caption_df = self.tokenizer.caption_df.drop(columns=['cap_no'])
+
+            tmp_df = pd.DataFrame({'id':[], 'caption':[]})
+            for i in self.df['id']:
+                a = caption_df[caption_df['id']==i].reset_index(drop=True)
+                tmp_df = pd.concat([tmp_df, a])
+            self.df = tmp_df.reset_index(drop=True)
+
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):
         image_path = self.df['id'][idx]
-        caption = self.tokenizer.caption_df[self.tokenizer.caption_df['id']==image_path].reset_index(drop=True)['caption'] # [0].lower().strip().split()
         image = Image.open(os.path.join(self.root, 'Flicker8k_Dataset', image_path))
         if self.tfms:
             image = self.tfms(image)
 
         if self.mode == 'train':
-            captions = random.choice(caption)
+            captions = self.df['caption'][idx]
             captions = captions.lower().strip().split()
             captions.insert(0, '<start>')
             captions.append('<end>')
@@ -39,6 +47,7 @@ class CaptionDataset(torch.utils.data.Dataset):
             caplens = torch.LongTensor([len(captions)])
 
         else:
+            caption = self.tokenizer.caption_df[self.tokenizer.caption_df['id']==image_path].reset_index(drop=True)['caption']
             captions = caption.tolist()
             for i, c in enumerate(captions):
                 temp = c.lower().strip().split()
